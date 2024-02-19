24e90c596cf0fb6c598140f85d9fbbbc5ad3a3c2
Gozuro <160263006+Gozuro@users.noreply.github.com>
2024-02-18 23:52:33 -0600

    Add files via upload

---
+++ main.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v5.9.6, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.9.6, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="https://r.mobirisesite.com/324010/assets/images/logo-h_llpt0ao0.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>Portafolio PWEBII</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons-bold/mobirise-icons-bold.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/parallax/jarallax.css">
+  <link rel="stylesheet" href="assets/web/assets/gdpr-plugin/gdpr-styles.css">
+  <link rel="stylesheet" href="assets/animatecss/animate.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Cairo:200,300,400,500,600,700,800,900&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cairo:200,300,400,500,600,700,800,900&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+</head>
+
+<body>
+  
+  <section data-bs-version="5.1" class="menu cid-s48OLK6784" once="menu" id="menu1-h">
+    
+    <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+        <div class="container-fluid">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="https://mobiri.se">
+                        <img src="https://r.mobirisesite.com/324010/assets/images/logo-h_llpt0ao0.png" alt="UPSLP" style="height: 4.7rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-primary display-7" href="https://mobiri.se">Administrador</a></span>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-success show text-primary display-4" href="..htmlP/index.php" data-bs-auto-close="outside" aria-expanded="false"><span class="mbri-home mbr-iconfont mbr-iconfont-btn" style="font-size: 20px;"></span>Inicio</a></li>
+                    <li class="nav-item dropdown"><a class="nav-link link text-success dropdown-toggle display-4" href="#" aria-expanded="false" data-toggle="dropdown-submenu" data-bs-toggle="dropdown" data-bs-auto-close="outside"><span class="mbri-edit mbr-iconfont mbr-iconfont-btn" style="font-size: 20px;"></span>Plan de materias</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown-undefined">
+                        <a class="text-success show dropdown-item text-primary display-4" href="NuevoPE.php" aria-expanded="false" data-bs-toggle="dropdown" data-bs-auto-close="outside"><p>&nbsp;&nbsp;&nbsp;&nbsp;<span class="mbri-file mbr-iconfont mbr-iconfont-btn" style="font-size: 25px;"></span>Nuevo</p></a>
+                    
+
+                
+                
+            </div>
+        </div>
+    </nav>
+</section>
+
+<section data-bs-version="5.1" class="header1 cid-tQPXJPhuWZ mbr-fullscreen mbr-parallax-background" id="header1-20">
+    <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(187, 187, 187);"></div>
+
+    <div class="container">
+        <div class="row justify-content-end">
+        <div >
+                <h1 class="mbr-section-title mbr-fonts-style mb-3 display-1"><strong>ALumno Jose de Jesus Gonzalez Rodrigues-171877</strong></h1>
+                
+                <p class="mbr-text mbr-fonts-style display-7">
+                   Universidad politecnica de San Luis Potosi </p>
+                   <p >Carrrera: Ingenieria en Tecnologias de la Informacion</p>                
+            
+                <h1><strong>Objetivo de la materia</strong></h1>
+                
+                <p>
+                El alumno desarrollará las habilidades necesarias en el uso dellenguaje PHP y Mysql,
+                 que le permitan analizar, diseñar y crear sitiosweb dinámicos que den solución a problemas reales, 
+                 utilizandoherramientas adecuadas para asegurar que cumplan con losrequerimientos y estándares de seguridad.
+                  Una vez creados, lospublicará y administrará.</p>                
+            
+                <h1><strong>Competencias</strong></h1>
+                
+                <p>
+                Determinar requerimientos de Información de la organización mediante instrumentos otécnicas de recopilación de información para obtener especificaciones técnicas de la basede datos a diseñar.
+                • Proponer diseño de base de datos mediante diagramas de bases datos que satisfagan losrequerimientos de la organización.
+                • Implementar bases de datos apegándose a la propuesta de diseño para satisfacerlasnecesidades de información de la organización.
+                • Mantener la operación de la base de datos apegándose a las políticas de seguridad y bajolos estándares reconocidos por la industria para su correcto desempeño.
+                • Determinar los requerimientos de información utilizando instrumentos de recolecciónde datos identificando políticas y necesidades de la organización para desarrollarsoluciones de software.
+                • Documentar los requerimientos de Información utilizando técnicas para su clasificación,representación y codificación para establecer un registro estandarizado derequerimientos.</p>
+                <p>• Modelar el sistema de información con base en la documentación derequerimientos para evaluar y mejorar el desempeño de los procesos actuales.• Proponer el sistema de información de acuerdo a la evaluación del modelo y a ladisponibilidad de recursos para satisfacer las necesidades de la organización.• Formular planes de desarrollo de sistemas con base en las estimaciones detiempo y recursos para alcanzar los objetivos acordados.• Ejecutar los planes de desarrollo de sistemas estableciendo mecanismos decontrol para verificar el cumplimiento de los objetivos de la organización.• Seleccionar las métricas de desempeño de acuerdo a los estándares reconocidosque aseguren la correcta operación del sistema de información.• Evaluar el desempeño del sistema de información de acuerdo a las métricasestablecidas para verificar su correcta operación.</p>          
+           
+                <h1><strong>Temas de la materia</strong></h1>
+                
+                <p>
+                1. Introducción a las aplicaciones WEB2. Bases de datos.3. Diseño físico relacional4. PHP5. Elementos Avanzados de Programación6. Seguridad en Aplicaciones Web</p>                
+            
+        </div>
+    </div>
+</section>
+
+<?php/*separacion*/?>
+
+<section data-bs-version="5.1" class="features1 cid-tQUhcN1l0z" id="features2-30"> 
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="item features-without-image mb-4 col-12 col-md-3 active">
+                <div class="item-wrapper">
+                    <div class="card-box align-center">
+                        <a href="NuevoPE.html"><span class="mbr-iconfont mbrib-edit" style="font-size: 70px;"></span></a>
+                        <h4 class="card-title align-center mbr-black mbr-fonts-style display-7">
+                            <strong><a href="NuevoPE.html" class="text-primary">Plan de Estudios</a></strong></h4>
+                    </div>
+                </div>
+            </div><div class="item features-without-image mb-4 col-12 col-md-3">
+                <div class="item-wrapper">
+                    <div class="card-box align-center">
+                        <a href="Kardex.html"><span class="mbr-iconfont mbrib-pages" style="font-size: 70px;"></span></a>
+                        <h4 class="card-title align-center mbr-black mbr-fonts-style display-7"><strong>Kardex</strong></h4>
+                    </div>
+                </div>
+            </div><div class="item features-without-image mb-4 col-12 col-md-3">
+                <div class="item-wrapper">
+                    <div class="card-box align-center">
+                        <a href="NuevoUsu.html"><span class="mbr-iconfont mbrib-users" style="font-size: 70px;"></span></a>
+                        <h4 class="card-title align-center mbr-black mbr-fonts-style display-7">
+                            <strong>Usuarios</strong></h4>
+                    </div>
+                </div>
+            </div><div class="item features-without-image mb-4 col-12 col-md-3">
+                <div class="item-wrapper">
+                    <div class="card-box align-center">
+                        <a href="Q&A.html"><span class="mbr-iconfont mbrib-help" style="font-size: 70px;"></span></a>
+                        <h4 class="card-title align-center mbr-black mbr-fonts-style display-7"><strong>Ayuda&nbsp;</strong></h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>   
+</section>
+
+<section data-bs-version="5.1" class="footer3 cid-s48P1Icc8J" once="footers" id="footer3-i">
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="row row-links">
+                <ul class="foot-menu">
+                 
+                <li class="foot-menu-item mbr-fonts-style display-7"></li><li class="foot-menu-item mbr-fonts-style display-7"><a href="InicioAdmin.html" class="text-success text-primary">Inicio</a></li><li class="foot-menu-item mbr-fonts-style display-7"><a href="https://www.upslp.edu.mx/upslp/" class="text-success text-primary">UPSLP</a></li></ul>
+            </div>
+            <div class="row social-row">
+                <div class="social-list align-right pb-2">
+                <div class="soc-item">
+                        <a href="https://www.tiktok.com/@upslp" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-tiktok socicon"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.facebook.com/upslp" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.youtube.com/@UPSLPoficial" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-youtube socicon"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.instagram.com/upslp_oficial/" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
+                        </a>
+                    </div></div>
+            </div>
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
+                    © Universidad Politécnica de San Luis Potosí<br><br></p>
+            </div>
+        </div>
+    </div>
+</section>   
+
+<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>  <script src="assets/parallax/jarallax.js"></script>  <script src="assets/web/assets/cookies-alert-plugin/cookies-alert-core.js"></script>  <script src="assets/web/assets/cookies-alert-plugin/cookies-alert-script.js"></script>  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/ytplayer/index.js"></script>  <script src="assets/dropdown/js/navbar-dropdown.js"></script>  <script src="assets/theme/js/script.js"></script>     <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i class="mbr-arrow-up-icon mbr-arrow-up-icon-cm cm-icon cm-icon-smallarrow-up"></i></a></div>
+    <input name="animation" type="hidden">
+  </body>
+</html>

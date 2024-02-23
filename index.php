eb67c560032f708a0f23ae46f43280679dbe573f
Gozuro <160263006+Gozuro@users.noreply.github.com>
2024-02-22 23:32:45 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Portafolio PWEBII 171877</title>
+    
+</head>
+<body>
+    <section id="texto">
+        <a href="https://www.upslp.edu.mx">
+          <img src="upslplog.png" alt="UPSLP" style="height: 4.7rem;">
+        </a>
+        <h1>Alumno: José de Jesús González Rodrígues-171877</h1>
+    </section>
+    <section id="texto">
+        <h2>Universidad politecnica de San Luis Potosi</h2>
+    </section>
+    <section id="texto">
+        <h2>Carrrera: Ingenieria en Tecnologias de la Informacion</h2>
+    
+        <h2><strong>Objetivo de la materia</strong></h2>
+        <p> El alumno desarrollará las habilidades necesarias en el uso dellenguaje PHP y Mysql,
+            que le permitan analizar, diseñar y crear sitios web dinámicos que den solución a problemas reales, 
+            utilizandoherramientas adecuadas para asegurar que cumplan con losrequerimientos y estándares de seguridad.
+             Una vez creados, lospublicará y administrará.</p>
+    </section>
+
+    <section id="listado">
+        <h2>Competencias</h2>
+        <ul>
+            <li>•Determinar requerimientos de Información de la organización mediante instrumentos otécnicas de recopilación de información para obtener especificaciones técnicas de la basede datos a diseñar.</li>
+            <li>•Proponer diseño de base de datos mediante diagramas de bases datos que satisfagan losrequerimientos de la organización.</li>
+            <li>•Implementar bases de datos apegándose a la propuesta de diseño para satisfacerlasnecesidades de información de la organización.</li>
+            <li>•Mantener la operación de la base de datos apegándose a las políticas de seguridad y bajolos estándares reconocidos por la industria para su correcto desempeño.</li>
+            <li>•Determinar los requerimientos de información utilizando instrumentos de recolecciónde datos identificando políticas y necesidades de la organización para desarrollarsoluciones de software.</li>
+            <li>•Documentar los requerimientos de Información utilizando técnicas para su clasificación,representación y codificación para establecer un registro estandarizado derequerimientos.</li>
+            <li>•Modelar el sistema de información con base en la documentación derequerimientos para evaluar y mejorar el desempeño de los procesos actuales.</li>
+            <li>•Proponer el sistema de información de acuerdo a la evaluación del modelo y a ladisponibilidad de recursos para satisfacer las necesidades de la organización.</li>
+            <li>•Formular planes de desarrollo de sistemas con base en las estimaciones detiempo y recursos para alcanzar los objetivos acordados.</li>
+            <li>•Ejecutar los planes de desarrollo de sistemas estableciendo mecanismos decontrol para verificar el cumplimiento de los objetivos de la organización.• Seleccionar las métricas de desempeño de acuerdo a los estándares reconocidosque aseguren la correcta operación del sistema de información.</li>
+            <li>•Evaluar el desempeño del sistema de información de acuerdo a las métricasestablecidas para verificar su correcta operación.</li>
+        
+        
+        </ul>
+    </section>
+
+    <section id="fotos">
+
+        <h2>Proyectos imagenes</h2>
+        <img class="preview-image" src="ruta_de_tu_imagen_1.jpg" alt="Descripción de la imagen 1">
+        <img class="preview-image" src="ruta_de_tu_imagen_2.jpg" alt="Descripción de la imagen 2">
+        <img class="preview-image" src="ruta_de_tu_imagen_3.jpg" alt="Descripción de la imagen 3">
+    </section>
+    
+</body>
+</html>
